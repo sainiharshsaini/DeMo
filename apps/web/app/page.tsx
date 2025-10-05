481f133f0c4aeae41a921cbf3e35cf144a5f94aa
HARSH SAINI <139774512+sainiharshsaini@users.noreply.github.com>
2025-10-05 19:55:51 +0530

    first commit

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { Card } from "@repo/ui/card";
+import { Gradient } from "@repo/ui/gradient";
+import { TurborepoLogo } from "@repo/ui/turborepo-logo";
+import { prisma } from "@repo/db";
+
+export default async function Page() {
+  
+  const user = await prisma.user.findFirst() 
+
+  return (
+    <div>
+      {user?.name ?? "No user added yet"}
+    </div>
+  );
+}
